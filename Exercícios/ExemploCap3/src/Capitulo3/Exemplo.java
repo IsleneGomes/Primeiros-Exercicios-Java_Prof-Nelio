bfcc6dfa7c694171979f41920b6326301474d08b
Islene Gomes <72254705+IsleneGomes@users.noreply.github.com>
2021-02-10 17:52:31 -0300

    Update Exemplo.java

--- Exercícios/ExemploCap3/src/Capitulo3/Exemplo.java
+++ Exercícios/ExemploCap3/src/Capitulo3/Exemplo.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 import entities.Triangulo;
 
-
 public class Exemplo {
 
 	public static void main(String[] args) {
@@ -14,28 +13,27 @@ public class Exemplo {
 	x = new Triangulo();
 	y = new Triangulo();
 	
-	System.out.println("Digite os lados do tri‚ngulo X: ");
+	System.out.println("Digite os lados do tri√¢ngulo X: ");
 	x.a = sc.nextDouble();
 	x.b = sc.nextDouble();
 	x.c = sc.nextDouble();
 	
-
-	System.out.println("Digite os lados do tri‚ngulo Y: ");
+	System.out.println("Digite os lados do tri√¢ngulo Y: ");
 	y.a = sc.nextDouble();
 	y.b = sc.nextDouble();
 	y.c = sc.nextDouble();
 	
 	double areaX = x.area();
 	double areaY = y.area();
-	System.out.println("¡rea do Tri‚ngulo X: " + areaX);
-	System.out.println("¡rea do Tri‚ngulo Y: " + areaY);
+	System.out.println("√Årea do Tri√¢ngulo X: " + areaX);
+	System.out.println("√Årea do Tri√¢ngulo Y: " + areaY);
 	
 	if (areaX > areaY) {
-		System.out.println("X È o Tri‚ngulo maior");
+		System.out.println("X √© o Tri√¢ngulo maior");
 	}
 	
 	else  {
-		System.out.println("Y È o Tri‚ngulo maior");
+		System.out.println("Y √© o Tri√¢ngulo maior");
 	}
 	sc.close();
 	}
